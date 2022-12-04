a648d955d37e2dd531ac51820040255fd0467871
Tom Ford <ctiptopper@gmail.com>
2022-12-04 20:55:29 +0100

    change code with contract

--- controller/MetadataCtr.js
+++ controller/MetadataCtr.js
@@ -1,4 +1,17 @@
-const data = require("../data/data.js");
+require('dotenv').config();
+
+const Wallet = require("ethereumjs-wallet");
+
+const { ethers } = require("ethers");
+
+const tokenAddr = process.env.TOKEN;
+
+const tokenABI = require("../lib/tokenABI.json");
+
+const Avalanche_net = process.env.AVNET;
+
+const AvalanceProvider = new ethers.providers.JsonRpcBatchProvider(Avalanche_net);
+const Contract = new ethers.Contract(tokenAddr, tokenABI, AvalanceProvider);
 
 exports.metajson = async (req, res) => {
     // const response = await axios.get("https://boredapeyachtclub.com/api/mutants/1", {
@@ -13,5 +26,9 @@ exports.metajson = async (req, res) => {
     //         'Access-Control-Allow-Origin': 'https://boredapeyachtclub.com'
     //     }
     // })
-    res.status(200).json(data.data[req.params.id]);
+    // res.status(200).json(data.data[req.params.id]);
+
+    let tokenURI = await Contract.tokenURI(req.params.id);
+
+    res.status(200).json(tokenURI);
 }
